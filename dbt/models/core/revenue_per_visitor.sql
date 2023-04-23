6a8f97aa917ae76c1047bcd47a3579355bacc0c3
fluentnumbers@gmail.com <fluentnumbers@gmail.com>
2023-04-23 16:15:09 +0000

    commit

--- dbt/models/core/revenue_per_visitor.sql
+++ dbt/models/core/revenue_per_visitor.sql
@@ -2,12 +2,12 @@
 
 select t1.day as day, daily_visitor, sum_of_revenue, round(sum_of_revenue/daily_visitor,2) as rpv
 from (
-    select format_date('%m-%d', event_time) as day, count(distinct user_id) as daily_visitor
+    select CAST(event_time as date) as day, count(distinct user_id) as daily_visitor
     from {{ref("data_clustered")}}
     group by day) as t1
 
 inner join (
-    select format_date('%m-%d', event_time) as day, sum(price) as sum_of_revenue
+    select CAST(event_time as date) as day, sum(price) as sum_of_revenue
     from {{ref("data_clustered")}}
     group by day) as t2
     
